0e6bb803adf8aab08ec9881506c3b593c533de3d
Janne Mattila <janne.mattila@hotmail.fi>
2022-08-16 18:24:57 +0300

    Add placeholder implementation structure

---
+++ src/Client/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Sockets;
+
+var cancellationToken = new CancellationTokenSource();
+
+Console.CancelKeyPress += (sender, e) =>
+{
+    cancellationToken.Cancel();
+};
+
+try
+{
+    var client = new TcpClient("localhost", 10_000);
+    while (client.Connected && !cancellationToken.IsCancellationRequested)
+    {
+        Console.Write(".");
+        await Task.Delay(1_000);
+    }
+
+    client.Close();
+}
+catch (ArgumentNullException e)
+{
+    Console.WriteLine("ArgumentNullException: {0}", e);
+}
+catch (SocketException e)
+{
+    Console.WriteLine("SocketException: {0}", e);
+}
---
+++ src/Server/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Net.Sockets;
+
+var cancellationToken = new CancellationTokenSource();
+var delay = 0;
+var lastReportedCount = -1;
+
+Console.CancelKeyPress += (sender, e) =>
+{
+    cancellationToken.Cancel();
+};
+
+Console.WriteLine("Starting server");
+var tcpListener = TcpListener.Create(10_000);
+tcpListener.Start();
+
+var clients = new List<TcpClient>();
+
+while (!cancellationToken.IsCancellationRequested)
+{
+    if (tcpListener.Pending())
+    {
+        var client = await tcpListener.AcceptTcpClientAsync(cancellationToken.Token);
+        if (client != null)
+        {
+            clients.Add(client);
+            delay = 0;
+        }
+    }
+    else
+    {
+        delay = Math.Min(delay + 10, 1000);
+        await Task.Delay(delay);
+
+        var currentCount = clients.Count;
+        if (currentCount != lastReportedCount)
+        {
+            lastReportedCount = currentCount;
+            Console.WriteLine($"{Environment.NewLine}{currentCount} connected.");
+        }
+        else
+        {
+            var disconnectedClients = new List<TcpClient>();
+            foreach (var client in clients)
+            {
+                if (!client.Connected)
+                {
+                    disconnectedClients.Add(client);
+                }
+            }
+
+            if (disconnectedClients.Any())
+            {
+                foreach (var disconnectedClient in disconnectedClients)
+                {
+                    clients.Remove(disconnectedClient);
+                }
+            }
+            Console.Write(".");
+        }
+    }
+}
